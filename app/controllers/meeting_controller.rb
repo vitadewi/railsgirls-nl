f1f84bf7fd07e0248157c8ce57b5091839db069a
VD <vitadewi.vanbeurden@gmail.com>
2013-03-03 12:48:10 +0100

    events for railsgirls

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -2,7 +2,7 @@ class MeetingController < ApplicationController
 
 	def index
 		@member = MeetupMember.for_member('47207762')
-		@events = MeetupEvent.for_group_name('appsterdam')
-		@group = MeetupGroup.for_group_name('appsterdam')
+		@events = MeetupEvent.for_group_name('RailsGirls-NL')
+		@group = MeetupGroup.for_group_name('RailsGirls-NL')
 	end
 end
